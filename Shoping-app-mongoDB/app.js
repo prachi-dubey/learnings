921000de4c26f18f4a43da3d32d820b041b578e9
prachi dubey <prachi@newput.com>
2023-08-28 10:26:30 +0530

    Initial experiments

---
+++ Shoping-app-mongoDB/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('648aae1f90e60f4c1ae34487')
+    .then(user => {
+      console.log('648aae1f90e60f4c1ae34487 :  ', user)
+      req.user = new User(user.name, user.email, user.cart, user._id);
+
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
